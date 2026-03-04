58ea25e843fc51c31202830aaaa87ef164c13a54
William James <willjames@protonmail.com>
2026-03-04 11:07:24 +0100

    added sampler file and file type param

---
+++ DSP_Sketchbook/Modules/Sampler.cpp
@@ -0,0 +1,8 @@
+//
+//  Sampler.cpp
+//  
+//
+//  Created by Billy James on 03/03/26.
+//
+
+#include "Sampler.hpp"
---
+++ DSP_Sketchbook/Modules/Sampler.hpp
@@ -0,0 +1,13 @@
+//
+//  Sampler.hpp
+//  
+//
+//  Created by Billy James on 03/03/26.
+//
+
+#ifndef Sampler_hpp
+#define Sampler_hpp
+
+#include <stdio.h>
+
+#endif /* Sampler_hpp */
